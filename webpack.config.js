51b18ff5f0f9a2ffe94b72c4cd0e6b64217bf516
Jordan Holmes <jordanstholmes@gmail.com>
2018-12-20 14:36:55 -0800

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,45 @@
-const webpack = require('webpack');
 const path = require('path');
-const dotenv = require('dotenv');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = env => {
-  const envFile = dotenv.config({ path: path.resolve(__dirname, `../.env`) }).parsed;
-  const envKeys = Object.keys(envFile).reduce((prev, next) => {
-    // eslint-disable-next-line
-    prev[`process.env.${next}`] = JSON.stringify(envFile[next]);
-    return prev;
-  }, {});
-  console.log(envKeys);
-  return {
-    entry: './client/index.jsx',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'public')
-    },
-    plugins: [new webpack.DefinePlugin(envKeys)],
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    watch: env.NODE_ENV === 'development',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    },
-    mode: env.NODE_ENV,
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          loader: 'style-loader'
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new Dotenv({
+      path: '../.env',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
         },
-        {
-          test: /\.css$/,
-          loader: 'css-loader',
-          query: {
-            modules: true,
-            localIdentName: '[name]__[local]___[hash:base64:5]'
-          }
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, loader: 'url-loader?limit=100000' },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
-        { test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/, loader: 'url-loader?limit=100000' },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          }
-        }
-      ]
-    }
-  };
+      },
+    ],
+  },
 };
