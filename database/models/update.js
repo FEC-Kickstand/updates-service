cbebcb9ad6b4841d4fcd7b7154f8c0651de78e6e
Jordan Holmes <jordanstholmes@gmail.com>
2019-02-05 15:49:39 -0800

    Fix bug where number of comments changes on Update rerender

--- database/models/update.js
+++ database/models/update.js
@@ -2,5 +2,6 @@ module.exports = (sequelize, DataTypes) => sequelize.define('update', {
   title: DataTypes.STRING,
   body: DataTypes.TEXT,
   likes: DataTypes.INTEGER,
+  comments: DataTypes.INTEGER,
   pubDate: DataTypes.DATE,
 });
